3d803135109b9010f02ac1fcf3ecbe716e862b2c
Olivier Piot <olivier.piot@aliceadsl.fr>
2018-10-05 13:00:21 +0200

    passing data via props

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {this.props.value}
         </button>
       );
     }
-  }
+}
   
-  class Board extends React.Component {
+class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+        return <Square value={i} />;
     }
   
     render() {
@@ -42,9 +42,9 @@ class Square extends React.Component {
         </div>
       );
     }
-  }
+}
   
-  class Game extends React.Component {
+class Game extends React.Component {
     render() {
       return (
         <div className="game">
@@ -58,12 +58,12 @@ class Square extends React.Component {
         </div>
       );
     }
-  }
+}
   
-  // ========================================
+// ========================================
   
-  ReactDOM.render(
+ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
+);
   
